dbafa7458739888a3f1d3f38e61d39cb2bf92e3a
Sean Murphy <sean@iamseanmurphy.com>
2025-03-12 07:18:05 +0000

    1.1.4

--- range-slider-addon-for-gravity-forms/gf-range-slider.php
+++ range-slider-addon-for-gravity-forms/gf-range-slider.php
@@ -1,39 +1,87 @@
 <?php
 /*
-Plugin Name: Range Slider Addon For Gravity Forms
+Plugin Name: Range Slider Addon for Gravity Forms
 Plugin Url: https://pluginscafe.com/plugin/range-slider-for-gravity-forms-pro/
-Version: 1.1.3
-Description: A simple and nice plugin to add range slider to pick data easily in gravity forms.
-Author: Pluginscafe
+Version: 1.1.4
+Description: A sleek, lightweight, and customizable range slider designed for selecting numbers or data within Gravity Forms.
+Author: PluginsCafe
 Author URI: https://pluginscafe.com
 License: GPLv2 or later
-Text Domain: gravityforms
+Text Domain: range-slider-addon-for-gravity-forms
 */
 if (!defined('ABSPATH')) {
     exit;
 }
 
-define('GF_NU_RANGE_SLIDER_ADDON_VERSION', '1.1.3');
+if (function_exists('rsfgf_fs')) {
+    rsfgf_fs()->set_basename(false, __FILE__);
+} else {
 
-add_action('gform_loaded', array('GF_NU_Range_Slider_AddOn_Bootstrap', 'load'), 5);
+    if (! function_exists('rsfgf_fs')) {
+        // Create a helper function for easy SDK access.
+        function rsfgf_fs() {
+            global $rsfgf_fs;
 
-class GF_NU_Range_Slider_AddOn_Bootstrap {
+            if (! isset($rsfgf_fs)) {
+                // Include Freemius SDK.
+                require_once dirname(__FILE__) . '/vendor/freemius/start.php';
+                $rsfgf_fs = fs_dynamic_init(array(
+                    'id'                  => '15078',
+                    'slug'                => 'range-slider-for-gravity-forms',
+                    'type'                => 'plugin',
+                    'public_key'          => 'pk_a6b08be24cb45ef6ac1489c6734c0',
+                    'is_premium'          => false,
+                    'premium_suffix'      => 'Pro',
+                    // If your plugin is a serviceware, set this option to false.
+                    'has_premium_version' => true,
+                    'has_addons'          => false,
+                    'has_paid_plans'      => true,
+                    'menu'                => array(
+                        'slug'           => 'range-slider-for-gravity-forms',
+                        'first-path'     => 'options-general.php?page=range-slider-for-gravity-forms',
+                        'contact'        => false,
+                        'support'        => false,
+                        'parent'         => array(
+                            'slug' => 'options-general.php',
+                        ),
+                        'is_live'        => true,
+                    ),
+                ));
+            }
 
-    public static function load() {
-
-        if (! method_exists('GFForms', 'include_addon_framework')) {
-            return;
+            return $rsfgf_fs;
         }
-        // are we on GF 2.5+
-        define('GFRS_GF_MIN_2_5', version_compare(GFCommon::$version, '2.5-dev-1', '>='));
 
-        require_once('class-nu-range-slider.php');
-        require_once('class-nu-range-slider-field.php');
-
-        GFAddOn::register('GFRangeSliderAddOn');
+        // Init Freemius.
+        rsfgf_fs();
+        // Signal that SDK was initiated.
+        do_action('rsfgf_fs_loaded');
     }
-}
 
-function gf_nu_range_slider() {
-    return GFRangeSliderAddOn::get_instance();
+    define('GF_NU_RANGE_SLIDER_ADDON_VERSION', '1.1.3');
+    define('GF_NU_RANGE_SLIDER_URL', plugin_dir_url(__FILE__));
+    add_action('gform_loaded', array('GF_NU_Range_Slider_AddOn_Bootstrap', 'load'), 5);
+
+    class GF_NU_Range_Slider_AddOn_Bootstrap {
+
+        public static function load() {
+
+            if (! method_exists('GFForms', 'include_addon_framework')) {
+                return;
+            }
+            // are we on GF 2.5+
+            define('GFRS_GF_MIN_2_5', version_compare(GFCommon::$version, '2.5-dev-1', '>='));
+
+            require_once('includes/fields/class-nu-range-slider.php');
+            require_once('includes/fields/class-nu-range-slider-field.php');
+
+            require_once('includes/admin/class-menu.php');
+
+            GFAddOn::register('GFRangeSliderAddOn');
+        }
+    }
+
+    function gf_nu_range_slider() {
+        return GFRangeSliderAddOn::get_instance();
+    }
 }
--- range-slider-addon-for-gravity-forms/readme.txt
+++ range-slider-addon-for-gravity-forms/readme.txt
@@ -1,19 +1,19 @@
-=== Range Slider AddOn for Gravity Forms ===
+=== Range Slider Addon for Gravity Forms ===
 Author: Pluginscafe
 Contributors: pluginscafe, kaisarahmmed
 Tags: gravity forms, range slider, slider input, wordpress slider, gravity form range slider
 Requires at least: 5.0
 Requires PHP: 5.6
-Tested up to: 6.7.1
-Stable tag: 1.1.3
+Tested up to: 6.7
+Stable tag: 1.1.4
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
 
 
-A simple and nice plugin to add range slider to pick number or customize data easily.
+Integrate your gravity forms with a smooth, lightweight, customizable range slider and unrivaled performance on both mobile and desktop.
 
 == Description ==
-A simple and nice plugin to add range slider to pick number or customize data easily. This plugin adds a responsive range slider field with smooth animation. The field comes with a lot of options that's controllable from the Gravity Forms UI.
+A sleek, lightweight, and customizable range slider designed for selecting numbers or data within Gravity Forms.. This plugin adds a responsive range slider field with smooth animation. The field comes with a lot of options that's controllable from the Gravity Forms UI.
 
 == Features ==
 * Add minimum and maximum number
@@ -33,6 +33,12 @@ A simple and nice plugin to add range slider to pick number or customize data ea
 Installation is fairly straight forward. Install it from the WordPress plugin repository. 
 
 == Change log ==
+= 1.1.4 =
+* Introduce Pro Version
+* Added Freemius SDK
+* Added Dashboard
+* Bug Fixs
+
 = 1.1.3 =
 * Compatibility with Gravity Forms 2.9
 * Bug Fixs
